c5a8ec363b5fffb01c7bdf059ae9a079a5c9db2b
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-03-13 08:35:09 +0000

    Added try except block

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,8 @@
-user_input = input("Please input something: ")
-print(user_input)
+try:
+    user_input = input("Please input something: ").strip()
+    if user_input:
+        print(user_input)
+    else:
+        print(1 / 0)
+except ZeroDivisionError:
+    print("Sorry, but you entered nothing!")
