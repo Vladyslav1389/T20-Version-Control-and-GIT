ebc776e01531ce1f2d61505bc6922bb4249ca988
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-03-14 21:46:43 +0000

    Added comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,13 @@
 def validation_empty_input() -> str:
+    """
+    Validates user input for emptiness.
+
+    :return: User input.
+    :rtype: str
+    """
+
+    # Creating a while loop to ask the user continuously until the user inputs
+    # something.
     while True:
         user_input = input("Please input something: ").strip()
         if user_input:
